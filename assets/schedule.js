a009cfa7a2e9e7ed2a9dc3d10eaa93069f9625ca
Bill Donovan <williamcdonovan@gmail.com>
2017-06-21 15:10:57 -0600

    making a mess of things

--- assets/schedule.js
+++ assets/schedule.js
@@ -17,14 +17,7 @@ var config = {
 var database = firebase.database();
 
 var ref = database.ref('trains');
-
-var train = {
-	name: Thomas,
-	destination: Sodor,
-	firstTrain: 07:30,
-	frequency: 30
-}
-
+var trains = [];
 
 // Functions
 
@@ -34,5 +27,26 @@ function submitTrain(form) {
 
 // Processes
 
+// Take the form field inputs and turn them into an object to be stored in firebase
+
+$('form').submit(function(e){
+	e.preventDefault();
+	var $inputs = $('#newTrain :input:not(:button)');
+	console.log($inputs);
+  	// var train = {
+  	// 	var name = $inputs[0].value,
+  	// 	"destination" = $inputs[1].value,
+  	// 	"firstTrain" = $inputs[2].value,
+  	// 	"frequency" = $inputs[3].value
+  	// };
+  	// console.log(train);
+    // $inputs.each(function() {
+    // var name = $(this)[0].id;
+  //  	values[id] = $(this).val();
+  //    $(this).val('');
+  //  });
+  // console.log(values);
+
+})
 
 })
